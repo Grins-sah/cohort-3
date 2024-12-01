26be9e6e5455194e0ab556b96a60943fc73030ea
Grins sah <163144974+Grins-sah@users.noreply.github.com>
2024-12-01 16:38:48 +0530

    Update index.ts

--- week17/src/index.ts
+++ week17/src/index.ts
@@ -1,5 +1,5 @@
 import  {Client} from 'pg'
-const pgClient = new  Client("postgresql://alpha_owner:2wHBpd5WSvFT@ep-billowing-lake-a5ho8cec.us-east-2.aws.neon.tech/alpha?sslmode=require");
+const pgClient = new  Client("");
 async function main(){
     await pgClient.connect();
     const response  = await pgClient.query("SELECT * FROM users;");
